0a72c6c321733cc6a9f691e4ea5b026b7a4d2027
adamlnly <kassiouiadam@gmail.com>
2025-02-21 16:36:40 +0100

    Add files via upload

---
+++ sae105/data.js
@@ -0,0 +1,29 @@
+var numCase = 0;
+
+var data = [
+    {
+        musique: "Heart to Heart",
+        descriptionMusicale: "Un morceau indie-pop doux et intime, caractéristique du style de Mac DeMarco. Avec sa voix apaisante et des guitares mélodieuses, il explore les thèmes de la vulnérabilité et de l'amour sincère.",
+        fichierAudio: "musiques/heart-to-heart-3.mp3"
+    },
+    {
+        musique: "Ma Meilleure ennemie",
+        descriptionMusicale: "Un duo captivant qui mêle les sensibilités poétiques de Pomme et les rythmes ingénieux de Stromae. La chanson aborde la dualité d’une relation intense, entre amour et rivalité, sur une composition émotive et raffinée.",
+        fichierAudio: "musiques/test.ogg" 
+    },
+    {
+        musique: "Like Him",
+        descriptionMusicale: "Un titre introspectif signé Tyler, The Creator, où il évoque avec sincérité ses doutes, ses aspirations, et son envie de changer pour ressembler à un modèle ou être meilleur, sur des beats minimalistes mais percutants.",
+        fichierAudio: "musiques/like-him.mp3"
+    },
+    {
+        musique: "Romantic Homicide",
+        descriptionMusicale: "Un morceau sombre et poignant du jeune artiste d4vd, combinant des éléments de bedroom pop et d'indie rock. La chanson exprime un désespoir mélancolique, explorant les émotions brutes liées à une rupture.",
+        fichierAudio: "musiques/romantic-homicide.mp3"
+    },
+    {
+        musique: "Love is only a feeling",
+        descriptionMusicale: "Cette musique est le chef-d'oeuvre de Luv Resval, un artiste connu pour son écriture riche et ses placements uniques. Cette musique contient un message qui met en avant le caractère éphémere du temps. Resval nous emmène dans son univers fantaisite et nous déconnecte de la réalité le temps d'instant.",
+        fichierAudio: "musiques/love is only.mp3"
+    }
+];
